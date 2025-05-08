5f9f155d71669137a6790cf578a14d24434cb2ba
TheProgxy <theproxy@gmail.com>
2025-05-08 08:58:57 +0200

    Initial Commit

---
+++ Lumina/LuminaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuminaApp.swift
+//  Lumina
+//
+//  Created by Emanuele Burberi on 08/05/25.
+//
+
+import SwiftUI
+
+@main
+struct LuminaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
